bea55d5e1d1e187729af5e255ecf86edd7a86d4a
zubiam <zmansoor@sfu.ca>
2020-12-01 17:42:43 -0800

    adding plan for the coding

---
+++ plan.py
@@ -0,0 +1,37 @@
+## STEP 1: CREATE A SCAFFOLD
+
+# 1. Log into Youtube
+# 2. Grab our liked videos 
+# 3. Create a new playlist
+# 4. Search for the song
+# 5. Add new song to the Spotify playlist
+
+class CreatePlaylist:
+    def __init__(self):
+        pass
+
+    # 1. Log into Youtube
+    def get_youtube_client(self):
+        pass
+
+    # 2. Grab our liked videos:
+    def get_liked_videos(self):
+        pass
+    
+    # 3. Create a new playlist:
+    def create_playlist(self):
+        pass
+
+    # 4. Search for the song
+    def get_spotify_uri(self):
+        pass
+
+    # 5. Add new song to Spotify playlist
+    def add_song_to_playlist(self):
+        pass
+
+## STEP 2: RESEARCH TO MAKE THIS SCAFFOLD A REALITY
+
+# 1. Youtube API
+# 2. Spotify Web API
+# 3. Youtube-dl library
